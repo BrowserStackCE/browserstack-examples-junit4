c27bd53541a4d7436170a248c9afbbaae6629281
anirudha13 <anirudha.khanna@gmail.com>
2021-04-08 10:51:59 +0530

    Cleaning up unwanted files

--- src/test/java/com/browserstack/suites/e2e/EndToEndTest.java
+++ src/test/java/com/browserstack/suites/e2e/EndToEndTest.java
@@ -1,7 +1,5 @@
 package com.browserstack.suites.e2e;
 
-
-
 import org.junit.Test;
 
 import com.browserstack.pages.Bag;
@@ -14,50 +12,46 @@ import com.browserstack.utils.Constants;
 import com.browserstack.utils.DriverFactory;
 import com.browserstack.utils.MarkTest;
 
-
-
-
 public class EndToEndTest extends DriverFactory {
-	
-	 
-	  @Test 
-	  // @Description("EndToEndTest")
-	  public void endToEndTest() throws Exception { 
-		  // TODO Auto-generated method stub
-	 
-		  HomePage homepage = new HomePage(driver);
-	  
-		  homepage.ClickSignInButton();
-	  
-		  Login login = new Login(driver);
-	  
-		  login.login();
-	  
-		  homepage.AddToCart();
-	    
-		  Bag bag = new Bag(driver);
-	  
-		  bag.ClickCheckoutButton();
-	  
-		  Checkout checkout = new Checkout(driver);
-	  
-		  checkout.EnterCheckoutDetails();
-	  
-		  Constants.ConfirmationMessage = CommonSteps.isConfirmationMessageDisplayed(driver);
-	  
-		  Constants.CheckNoOfOrders = CommonSteps.checkNoOfItems(driver);
- 
-		  Confirmation confirmation = new Confirmation(driver);
-	  
-		  confirmation.ClickContinueShoppingButton();
-	  
-		  homepage.ClickOrderButton();
-	  
-		  MarkTest.MarkEndToEndTest(driver, Constants.ConfirmationMessage, Constants.CheckNoOfOrders);
-	 
-	 
 
-	  }
+
+    @Test
+    // @Description("EndToEndTest")
+    public void endToEndTest() throws Exception {
+        // TODO Auto-generated method stub
+
+        HomePage homepage = new HomePage(driver);
+
+        homepage.ClickSignInButton();
+
+        Login login = new Login(driver);
+
+        login.login();
+
+        homepage.AddToCart();
+
+        Bag bag = new Bag(driver);
+
+        bag.ClickCheckoutButton();
+
+        Checkout checkout = new Checkout(driver);
+
+        checkout.EnterCheckoutDetails();
+
+        Constants.ConfirmationMessage = CommonSteps.isConfirmationMessageDisplayed(driver);
+
+        Constants.CheckNoOfOrders = CommonSteps.checkNoOfItems(driver);
+
+        Confirmation confirmation = new Confirmation(driver);
+
+        confirmation.ClickContinueShoppingButton();
+
+        homepage.ClickOrderButton();
+
+        MarkTest.MarkEndToEndTest(driver, Constants.ConfirmationMessage, Constants.CheckNoOfOrders);
+
+
+    }
 
 
 }
