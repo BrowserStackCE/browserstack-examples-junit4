9a9c87f3c01761534dc670d1df36fce482cb36a7
adarshTS <adarsh.ts8@gmail.com>
2023-08-23 00:26:46 +0530

    added localTest

--- src/test/java/com/browserstack/suites/localTest/BStackLocalTest.java
+++ src/test/java/com/browserstack/suites/localTest/BStackLocalTest.java
@@ -1,14 +1,14 @@
 package com.browserstack.suites.localTest;
 
 import com.browserstack.utils.BaseTest;
-import org.testng.annotations.Test;
+import org.junit.Test;
 
 import static junit.framework.TestCase.assertTrue;
 
 public class BStackLocalTest extends BaseTest {
     @Test
     public void test() {
-        webDriver.get("http://bs-local.com:45454/");
+        webDriver.get("http://localhost:45454/");
         assertTrue("Local content not validated!", webDriver.getTitle().contains("BrowserStack Local"));
     }
 }
--- src/test/java/com/browserstack/utils/BaseTest.java
+++ src/test/java/com/browserstack/utils/BaseTest.java
@@ -1,34 +1,31 @@
 package com.browserstack.utils;
 
 import org.apache.commons.lang3.StringUtils;
-import org.junit.After;
-import org.junit.Before;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class BaseTest {
 
-    public WebDriver webDriver;
+    public static WebDriver webDriver;
 
-    @Before
-    public void init() {
-//        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
-//        webDriver = new ChromeDriver();
-
-        if (StringUtils.equalsIgnoreCase(System.getProperty("browserstack-local"),"true")) {
+    @BeforeClass
+    public static void init() {
+        if (StringUtils.equalsIgnoreCase(System.getProperty("browserstack-local"), "true")) {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
             webDriver = new ChromeDriver();
-
         } else {
             System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
             webDriver = new ChromeDriver();
             webDriver.get("https://bstackdemo.com");
         }
-
     }
 
-    @After
-    public void teardown() {
-        webDriver.quit();
+    @AfterClass
+    public static void teardown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
     }
 }
